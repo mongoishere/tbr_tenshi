42e4ab204a7eaae9ed3dfe1d433bbe938a4af2f4
Datboi <ilopilop538@gmail.com>
2019-01-23 08:32:22 -0600

    Extended googleDB and management functions

--- cogs/googleDB.py
+++ cogs/googleDB.py
@@ -2,8 +2,14 @@ import gspread
 import asyncio
 import discord
 import random
+import os
+import requests
+import tempfile
 import configparser
+from io import BytesIO
+from PIL import Image
 from discord.ext import commands
+from threading import Thread
 from oauth2client.service_account import ServiceAccountCredentials
 
 
@@ -12,7 +18,8 @@ scope = ['https://spreadsheets.google.com/feeds',
 
 spreadsheets = [
     "TBR Current Members (1/22/19)",
-    "TBR Battle Data"
+    "TBR Battle Data",
+    "Clan Index"
 ]
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name('cogs/Tenshi-fcd293624b99.json', scope)
@@ -66,9 +73,24 @@ class GoogleDB(object):
             if any(t in lowercase_enemy for t in arg_list):
                 records_found+=1; return_records.append(battle_records[i])        
         
-        await self.bot.say(f"`{records_found}` Battles Found")
+        await self.bot.say(f"`{records_found}` Battle(s) Found")
         print(return_records)
         return return_records
+
+    async def findClan(self, search_type, *args):
+
+        row_count = self.loaded_sheets['sh2'].row_count
+        clan_records = self.loaded_sheets['sh2'].get_all_records()
+        arg_list = list(map(str.lower, [list(arg) for arg in args][0]))
+        records_found, return_records = 0, []
+
+        for i in range(row_count - 1):
+            lowercase_enemy = clan_records[i][search_type].lower()
+            if all(t in lowercase_enemy for t in arg_list):
+                records_found+=1; return_records.append(clan_records[i])        
+        
+        await self.bot.say(f"`{records_found}` Clan(s) Found")
+        return return_records, records_found
 
     @commands.command(pass_context=True)
     async def retrieveSomething(self, ctx):
@@ -78,17 +100,18 @@ class GoogleDB(object):
     async def memberinfo(self, ctx, *member_name):
         member_data = await self.findMember(' '.join(member_name), ctx.message.author)
         if(member_data):
-            return await self.bot.say(
-                f'⇨ RECORD FOUND ⇦\n\n\
-            Member Name: `{member_data[0]}`\n\
-            Date Joined: `{member_data[3]}`\n\
-            ---------------------------\n\
-            ⇀ Acitivity Status: `{member_data[4]}`\n\
-            ⇀ Family Status `{member_data[7]}`\n\
-            ⇀ Military Status: `{member_data[6]}`\n\
-            ⇀ Associated Chapters: `{member_data[-2]}`\n\
-            ⇀ Special Skills/Qualities: `{member_data[-1]}`'
+            member_chunk = (
+                f"⇨ RECORD FOUND ⇦\n\n"
+                f"Member Name: `{member_data[0]}`\n"
+                f"Date Joined: `{member_data[3]}`\n"
+                f"---------------------------\n"
+                f"⇀ Acitivity Status: `{member_data[4]}`\n"
+                f"⇀ Family Status `{member_data[7]}`\n"
+                f"⇀ Military Status: `{member_data[6]}`\n"
+                f"⇀ Associated Chapters: `{member_data[-2]}`\n"
+                f"⇀ Special Skills/Qualities: `{member_data[-1]}`\n"
             )
+            return await self.bot.say(member_chunk)
         else:
             uname = str(ctx.message.author).split('#')[0]
             not_found = [
@@ -119,9 +142,9 @@ class GoogleDB(object):
                 await self.bot.say(f"Displaying Battles by {search_tag}")
                 battle_data = await self.findBattle(search_tag, args)
 
-        if not(battle_data): await self.bot.say(random.randint(0, len(error_responses)-1))
+        if not(battle_data): await self.bot.say(error_responses[random.randint(0, len(error_responses)-1)])
 
-        if(battle_data):
+        else:
             battle_string = ''
             for ind, battle in enumerate(battle_data):
                 if(ind > 5): 
@@ -141,8 +164,59 @@ class GoogleDB(object):
                 )
                 battle_string += battle_chunk
                 
-                return await self.bot.say(battle_string)
+            return await self.bot.say(battle_string)
 
+    @commands.command(pass_context=True)
+    async def claninfo(self, ctx, ciCommand, *args):
+        clan_data = None
+        clan_strings = {
+            ('n', 'name'): 'Clan Name',
+            ('a', 'abbrev'): 'Abbreviation',
+            ('c', 'community'): 'Communities'
+        }
+        error_responses = [
+            'I can\'t match any clans',
+            '....no, sorry, nothing',
+            'Hmmmmm I can\'t find them',
+            'I don\'t know about that one'
+        ]
+
+        for key in clan_strings:
+            if(ciCommand in key):
+                if not(args): await self.bot.say(f"`$claninfo [{key[0]}|{key[1]}] <{clan_strings[key]}>`"); return
+                search_tag = clan_strings[key]
+                await self.bot.say(f"Displaying Clans by {search_tag}")
+                clan_data, records_found = await self.findClan(search_tag, args)
+        
+        if not(clan_data): await self.bot.say(error_responses[random.randint(0, len(error_responses)-1)])
+        else:
+            clan_string = ''
+            for ind, clan in enumerate(clan_data):
+                
+                if(records_found == 1) and clan['Clan Emblem']:
+                    url_info = (
+                        f"{clan['Website']}\n"
+                        f"{clan['YouTube']}\n"
+                    )
+                    e = discord.Embed()
+                    e.set_image(url=clan['Clan Emblem'])
+                    response = requests.get(clan['Clan Emblem'])
+                    await self.bot.say(url_info)
+                    await self.bot.send_message(ctx.message.channel, embed=e)
+                if(ind > 5): 
+                    await self.bot.say(clan_string)
+                    clan_string = ''
+
+                clan_chunk = (f"-------------------------------------------\n"
+                    f"Clan Name: `{clan['Clan Name']} (abbrev. {clan['Abbreviation']})`\n"
+                    f"Led By: `{clan['Leader[s]']}`\n"
+                    f"Since: `{clan['Established']}`\n"
+                    f"Communities: `{clan['Communities']}`\n"
+                    f"Clan Type: `{clan['Clan Type']}`\n"
+                    f"`{clan['Description']}`\n"
+                )
+                clan_string += clan_chunk
+            return await self.bot.say(clan_string)
         
 
 # This setup function is mandatory for all cogs
--- cogs/management.py
+++ cogs/management.py
@@ -1,6 +1,7 @@
 import gspread
 import asyncio
 import discord
+import random
 import configparser
 from discord.ext import commands
 
@@ -11,9 +12,43 @@ class Management(object):
         self.authorized_users = [
             'Dakurochi#0719'
         ]
+    
+    async def on_ready(self):
+        self.avail_servers = {}
+        self.avail_channels = {}
+        for server in self.bot.servers:
+            self.avail_servers[server.name] = server.id
+            for channel in server.channels:
+                self.avail_channels[channel.name] = channel.id
+        #import pdb; pdb.set_trace()
 
     async def on_message(self, message):
         print(message.author)
+
+    async def on_member_update(self, before, after):
+        greet_trigger = bool(random.getrandbits(1))
+        if(greet_trigger):
+            #import pdb; pdb.set_trace()
+            mention = after.mention
+            greetings = [
+                f"{mention} Hallo there :heart:",
+                f"{mention} Welcome back UwU",
+                f"{mention} Oh, hey there!",
+                f"Hey {mention} is here!",
+                f"{mention} Home sweet home"
+            ]
+
+            print(before.name)
+            if str(before.status) == "offline":
+                if str(after.status) == "online":
+                    target_channel = self.bot.get_channel(self.avail_channels['general'])
+                    await self.bot.send_message(target_channel, greetings[random.randint(0, len(greetings)-1)])
+
+    @commands.command(pass_context=True)
+    async def channelTest(self, ctx):
+        import pdb; pdb.set_trace()
+        channel = discord.utils.get(ctx.messaserver.channels, name='Foo', type=ChannelType.voice)
+        
 
     @commands.command(pass_context=True)
     async def send_message(self, ctx, channel_id, *args):
